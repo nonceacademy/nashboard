04b1412332c509c2a750ef14289519683e9c465f
0xanik <6179503+0xanik@users.noreply.github.com>
2022-12-22 04:09:11 +0330

    Customize theme

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -9,7 +9,14 @@ const config = {
 				sans: ["Inter var", ...defaultConfig.fontFamily.sans],
 				mono: ["Fira Code", ...defaultConfig.fontFamily.mono],
 				sartoshi: ["Sartoshi Script", ...defaultConfig.fontFamily.sans],
-			}
+			},
+			colors: {
+				primary: '#171717',
+				secondary: '#da0037',
+				title: '#01daa4',
+				message: '#cccccc'
+				
+			},
 		},
 		screens: {
 			// for heatmap
